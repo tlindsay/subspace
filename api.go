1be9b6a59aa346069d88a51ff33cc7068fa0fc0b
Patrick Lindsay <pat@thatdarnpat.com>
2022-08-11 13:34:48 -0500

    Refactor API module a bit

--- api.go
+++ api.go
@@ -9,73 +9,81 @@ import (
 	"strings"
 )
 
-type handler struct{}
-
 func StartServer(port int) {
-	h := newHandler()
-	fmt.Printf("Opening hailing frequencies on port %d...", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), h))
+	fmt.Printf("Opening hailing frequencies on port %d...\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), Handler()))
 }
 
-func newHandler() http.Handler {
-	return handler{}
-}
-
-func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/characters" {
-		j, err := json.Marshal(ListAllCharacters())
-		if err != nil {
-			http.Error(w, "Unknown error", 500)
+func Handler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/characters" {
+			j, err := json.Marshal(ListAllCharacters())
+			if err != nil {
+				http.Error(w, "Unknown error", 500)
+				return
+			}
+			w.Header().Add("Content-Type", "application/json")
+			w.Write(j)
 			return
 		}
-		w.Header().Add("Content-Type", "application/json")
-		w.Write(j)
-		return
-	}
-	q := r.URL.Query()
-	numP, err := strconv.Atoi(q.Get("paragraphs"))
-	if err != nil {
-		http.Error(w, "Bad value for param \"paragraphs\"", 400)
-		return
-	} else if numP < 1 {
-		numP = 1
-	}
-	numL, err := strconv.Atoi(q.Get("lines"))
-	if err != nil {
-		http.Error(w, "Bad value for param \"lines\"", 400)
-		return
-	} else if numL < 1 {
-		numL = 1
-	}
-	char := r.URL.Query().Get("character")
-	if char == "" {
-		char = "picard"
-	}
 
-	if _, exist := CHARACTERS[char]; !exist {
-		http.Error(w, "Character not supported. See valid characters at /characters", 400)
-		return
-	}
+		q := r.URL.Query()
 
-	output, err := MakeItSo(numP, numL, char)
+		numP, err := strconv.Atoi(q.Get("paragraphs"))
+		if err != nil {
+			http.Error(w, "Bad value for param \"paragraphs\"", 400)
+			return
+		} else if numP < 1 {
+			numP = 1
+		}
 
-	if err != nil {
-		log.Printf("Unknown error occurred: %s\n", err)
-		http.Error(w, "Unknown error", 500)
-		return
-	}
+		numL, err := strconv.Atoi(q.Get("lines"))
+		if err != nil {
+			http.Error(w, "Bad value for param \"lines\"", 400)
+			return
+		} else if numL < 1 {
+			numL = 1
+		}
 
-	if r.Header.Get("Content-Type") == "application/json" {
-		j, err := json.Marshal(output)
+		char := r.URL.Query().Get("character")
+		if char == "" {
+			char = "picard"
+		}
+		if _, exist := CHARACTERS[char]; !exist {
+			http.Error(w, "Character not supported. See valid characters at /characters", 400)
+			return
+		}
+
+		output, err := MakeItSo(numP, numL, char)
+
 		if err != nil {
 			log.Printf("Unknown error occurred: %s\n", err)
 			http.Error(w, "Unknown error", 500)
 			return
 		}
-		w.Header().Add("Content-Type", "application/json")
-		w.Write(j)
-		return
-	}
 
-	w.Write([]byte(strings.Join(output, "\n")))
+		if r.Header.Get("Content-Type") == "application/json" {
+			type JsonMeta struct {
+				Paragraphs int `json:"numParagraphs"`
+				Lines      int `json:"linesPerParagraph"`
+			}
+			type JsonResponse struct {
+				Character string   `json:"character"`
+				Text      []string `json:"text"`
+				Meta      JsonMeta `json:"meta"`
+			}
+
+			j, err := json.Marshal(JsonResponse{Character: char, Text: output, Meta: JsonMeta{Lines: numL, Paragraphs: numP}})
+			if err != nil {
+				log.Printf("Unknown error occurred: %s\n", err)
+				http.Error(w, "Unknown error", 500)
+				return
+			}
+			w.Header().Add("Content-Type", "application/json")
+			w.Write(j)
+			return
+		}
+
+		w.Write([]byte(strings.Join(output, "\n")))
+	}
 }
