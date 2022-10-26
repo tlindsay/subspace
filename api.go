39c9910fd6acb7df8303cb086def714651524d40
Patrick Lindsay <pat@thatdarnpat.com>
2022-10-26 14:53:44 -0500

    Add more logging

--- api.go
+++ api.go
@@ -4,10 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/tlindsay/subspace/response"
 )
@@ -77,7 +79,14 @@ func Handler() http.HandlerFunc {
 
 		char := r.URL.Query().Get("character")
 		if char == "" {
-			char = "picard"
+			logInfo.Println("Selecting random character...")
+			rand.Seed(time.Now().Unix())
+			charKeys := []string{}
+			for key, _ := range CHARACTERS {
+				charKeys = append(charKeys, key)
+			}
+			idx := rand.Intn(len(charKeys))
+			char = charKeys[idx]
 		}
 		if _, exist := CHARACTERS[char]; !exist {
 			http.Error(w, "Character not supported. See valid characters at /characters", 400)
@@ -85,6 +94,7 @@ func Handler() http.HandlerFunc {
 			return
 		}
 
+		logInfo.Printf("Getting lines for character: %s\n", char)
 		output, err := MakeItSo(numP, numL, char)
 
 		if err != nil {
@@ -93,6 +103,9 @@ func Handler() http.HandlerFunc {
 			return
 		}
 
+		logInfo.Printf("Returning data: %s\n", output[0])
+		logInfo.Printf("Paragraphs: %d, Lines: %d\n", numP, numL)
+
 		if r.Header.Get("Accept") == "application/json" {
 			j, err := json.Marshal(response.JsonResponse{Character: char, Text: output, Meta: response.JsonMeta{Lines: numL, Paragraphs: numP}})
 			if err != nil {
@@ -100,11 +113,11 @@ func Handler() http.HandlerFunc {
 				http.Error(w, "Unknown error", 500)
 				return
 			}
+
 			w.Header().Add("Content-Type", "application/json")
 			w.Write(j)
-			return
+		} else {
+			w.Write([]byte(strings.Join(output, "\n")))
 		}
-
-		w.Write([]byte(strings.Join(output, "\n")))
 	}
 }
